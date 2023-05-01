0b9f283383ac678f5948ddce9fa05c1c420b726f
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2023-05-01 16:05:44 +0800

    Initial commit

---
+++ curd/handler.go
@@ -0,0 +1,41 @@
+package curd
+
+import (
+	"github.com/gogf/gf/v2/database/gdb"
+	"github.com/gogf/gf/v2/os/gtime"
+)
+
+func CheckUserId(user int) func(m *gdb.Model) *gdb.Model {
+	return func(m *gdb.Model) *gdb.Model {
+		return m.Where("id", user)
+	}
+}
+
+func CheckUser(user int) func(m *gdb.Model) *gdb.Model {
+	return func(m *gdb.Model) *gdb.Model {
+		return m.Where("user", user)
+	}
+}
+
+func Order(order string, col ...string) func(m *gdb.Model) *gdb.Model {
+	return func(m *gdb.Model) *gdb.Model {
+		if order == "" {
+			return m
+		}
+		c := "create_at"
+		if len(col) > 0 {
+			c = col[0]
+		}
+		return m.Order(c, order)
+	}
+}
+
+func TimeRange(s, e any) func(m *gdb.Model) *gdb.Model {
+	return func(m *gdb.Model) *gdb.Model {
+		return m.WhereBetween("create_at", s, e)
+	}
+}
+
+func TimeToday() func(m *gdb.Model) *gdb.Model {
+	return TimeRange(gtime.Now().StartOfDay(), gtime.Now())
+}
