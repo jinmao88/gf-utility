55d69cb64bc1bd781c9c728aee051d4deba4e2ee
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2023-07-20 22:20:46 +0800

    抽离中间件

---
+++ middleware/cors.go
@@ -0,0 +1,17 @@
+package middleware
+
+import "github.com/gogf/gf/v2/net/ghttp"
+
+// 允许接口跨域请求
+func CORS(r *ghttp.Request) {
+	corsOptions := r.Response.DefaultCORSOptions()
+	corsOptions.AllowHeaders += "Access-Token"
+	r.Response.CORS(corsOptions)
+	r.Middleware.Next()
+}
+
+// 全局获取context
+func Ctx(r *ghttp.Request) {
+	r.SetParam("ctx", r.GetCtx())
+	r.Middleware.Next()
+}
