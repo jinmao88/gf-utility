d6a82119bf949b0e6d159ed373264ac14739420f
jinmao <jinmao88@qq.com>
2024-10-02 20:38:36 +0800

    feat: Options 接口支持附带额外数据

--- curd/options.go
+++ curd/options.go
@@ -4,12 +4,14 @@ type Options interface {
 	GetOptionLabel() string
 	GetOptionValue() string
 	GetOptionKey() string
+	GetData() interface{}
 }
 
 type Option struct {
-	Label string `json:"label"`
-	Value string `json:"value"`
-	Key   string `json:"key"`
+	Label string      `json:"label"`
+	Value string      `json:"value"`
+	Key   string      `json:"key"`
+	Data  interface{} `json:"data"`
 }
 
 func BuildOptions[T Options](a []T) (res []Option) {
@@ -19,6 +21,7 @@ func BuildOptions[T Options](a []T) (res []Option) {
 			Label: t.GetOptionLabel(),
 			Value: t.GetOptionKey(),
 			Key:   t.GetOptionKey(),
+			Data:  t.GetData(),
 		})
 	}
 	return
